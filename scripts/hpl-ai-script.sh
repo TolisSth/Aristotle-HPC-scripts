8d1d2c0c25da11b61ccb76b28c69b6fc5e155154
Apostolos Chalis <tolishalis@gmail.com>
2024-04-01 23:33:38 +0300

    Removed extra packages installation loop

--- scripts/hpl-ai-script.sh
+++ scripts/hpl-ai-script.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Function to check and install packages
-check_and_install() {
-    local package_name="$1"
-    rpm -q "$package_name" &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Installing $package_name..."
-        sudo yum install -y "$package_name"   # On CentOS 7. It is to be noted that in the time of writting this is the Aristotle Cent os version
-        # sudo dnf install -y "$package_name"  # On CentOS 8 or later
-    else
-        echo "$package_name is already installed."
-    fi
-}
-
 # Function to download and install OpenMPI
 install_openmpi() {
     local version="3.1.4"
@@ -34,20 +21,12 @@ install_openmpi() {
     rm -rf "openmpi-$version" "openmpi-$version.tar.gz"
 }
 
-# List of required packages
-packages=("blas-devel" "other_package_name" "another_package_name")
-
-# Install required packages
-for package in "${packages[@]}"; do
-    check_and_install "$package"
-done
-
 # Install OpenMPI
 install_openmpi
 
-echo "All required packages and OpenMPI installed successfully."
+echo "OpenMPI installed successfully."
 
-# clone hpl repo and run it
+# clone HPL-AI repo and run it
 git clone https://github.com/RIKEN-RCCS/hpl-ai.git
 cd hpl-ai
 make driver.out
